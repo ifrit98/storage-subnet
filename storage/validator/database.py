b3dac833ee17bd4eb3e936a3aa3bdf48bd8a095f
ifrit98 <ifrit98@gmail.com>
2023-11-24 14:26:47 +0000

    port remaining code changes

--- storage/validator/database.py
+++ storage/validator/database.py
@@ -228,3 +228,36 @@ def calculate_total_network_storage(database):
         # Grab storage for that hotkey
         total_storage += calculate_total_hotkey_storage(database, hotkey)
     return total_storage
+
+
+def get_miner_statistics(database: redis.Redis) -> Dict[str, Dict[str, str]]:
+    """
+    Retrieves statistics for all miners in the database.
+    Parameters:
+        database (redis.Redis): The Redis client instance.
+    Returns:
+        A dictionary where keys are hotkeys and values are dictionaries containing the statistics for each hotkey.
+    """
+    return {
+        key.decode("utf-8").split(":")[-1]: {
+            k.decode("utf-8"): v.decode("utf-8")
+            for k, v in database.hgetall(key).items()
+        }
+        for key in database.scan_iter(f"stats:*")
+    }
+
+
+def get_redis_db_size(database: redis.Redis) -> int:
+    """
+    Calculates the total approximate size of all keys in a Redis database.
+    Parameters:
+        database (int): Redis database
+    Returns:
+        int: Total size of all keys in bytes
+    """
+    total_size = 0
+    for key in database.scan_iter("*"):
+        size = database.execute_command("MEMORY USAGE", key)
+        if size:
+            total_size += size
+    return total_size
