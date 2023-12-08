7685ef1d075d8dc6f2c1aa7f6a6c2026001f9ba4
ifrit98 <ifrit98@gmail.com>
2023-12-08 22:45:19 +0000

    licensing

--- demo/verify_storage.py
+++ demo/verify_storage.py
@@ -1,3 +1,21 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+# Copyright © 2023 philanthrope
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 with open("test.txt", "rb") as f:
     x1 = f.read()
 
@@ -18,34 +36,3 @@ with open("/home/phil/.bittensor/storage/test100mb", "rb") as f:
 print("\nOriginal:", x1[:64])
 print("Stored  :", x2[:64])
 print("Test 100mb file equivalent?:", x1 == x2)
-
-
-# Clear storage
-import aioredis
-
-r0 = aioredis.StrictRedis(db=0)
-r1 = aioredis.StrictRedis(db=1)
-
-
-async def flush():
-    await r0.flushdb()
-    await r1.flushdb()
-
-
-# await flush()
-
-
-print(await r0.keys())
-print(await r1.keys())
-
-from storage.validator.database import *
-from storage.validator.utils import *
-
-full_hash = (
-    "10640854667234103088456065167400853561871673685400658931716168102369399248095"
-)
-md = await get_ordered_metadata(full_hash, r1)
-print(md)
-
-for d in md:
-    print(d["chunk_hash"], d["hotkeys"])
