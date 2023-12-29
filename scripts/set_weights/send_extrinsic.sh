8f7b7d1c222bfca9547993f349dfbe766642f2d1
eduardo <garciaruiz.edu@gmail.com>
2023-12-29 04:14:52 +0100

    fixi relativ path in script

--- scripts/set_weights/send_extrinsic.sh
+++ scripts/set_weights/send_extrinsic.sh
@@ -4,4 +4,4 @@ WALLET_NAME=$1
 WALLET_HOTKEY=$2
 SUBTENSOR_NETWORK=$3
 
-python ./scripts/send_extrinsic.py --wallet $WALLET_NAME --hotkey $WALLET_HOTKEY -network $SUBTENSOR_NETWORK
+python ./send_extrinsic.py --wallet $WALLET_NAME --hotkey $WALLET_HOTKEY -network $SUBTENSOR_NETWORK
