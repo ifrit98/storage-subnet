2ddeb5834f57d2ca4b921ef1a7470ffe86419cfd
eduardo <garciaruiz.edu@gmail.com>
2023-12-29 02:37:54 +0100

    Adding --miner.set_weights_at_start option to specify if we should set weights when the miner is starting the process

--- storage/miner/run.py
+++ storage/miner/run.py
@@ -64,7 +64,10 @@ def run(self):
         exit()
 
     # --- Run until should_exit = True.
-    self.last_epoch_block = self.subtensor.get_current_block()
+    if self.config.miner.set_weights_at_start:
+        self.last_epoch_block = -1
+    else:
+        self.last_epoch_block = self.subtensor.get_current_block()
     bt.logging.info(f"Miner starting at block: {self.last_epoch_block}")
 
     # This loop maintains the miner's operations until intentionally stopped.
