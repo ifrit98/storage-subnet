5465a82d7e7c55de18dd3ee89a2c55c215fb8593
ifrit98 <ifrit98@gmail.com>
2024-01-02 21:40:15 +0000

    reduce alpha to reduce variance of rewards over time

--- storage/validator/config.py
+++ storage/validator/config.py
@@ -207,7 +207,7 @@ def add_args(cls, parser):
         "--neuron.moving_average_alpha",
         type=float,
         help="Moving average alpha parameter, how much to add of the new observation.",
-        default=0.05,
+        default=0.02,
     )
     parser.add_argument(
         "--neuron.semaphore_size",
